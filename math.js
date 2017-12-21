a45d94809a278980ca6ec9c3eac07fe68c388b8c
hail27369 <32968932+hail27369@users.noreply.github.com>
2017-12-21 16:44:08 -0500

    Update math.js
    -Added randomFloat function
    -Fixed a bug in randomInt function where the max argument value would previously never be included in the random number generation range
    -Added error messages that are thrown in cases where the min argument is not less than the max argument

--- math.js
+++ math.js
@@ -1,4 +1,21 @@
 //returns a random integer between the specified min and max values
 function randomInt(min, max){
-	return Math.floor(Math.random() * (max - min)) + min;
+	if(min < max){
+		return Math.floor(Math.random() * ((max + 1) - min)) + min;
+	}
+	else{
+		throw 'Error: randomInt min value must be less than max value';
+	}
+}
+
+//Returns a random number that is formatted to a given amount of decimals between a minimum and maximum value
+function randomFloat(min, max, decimals){
+	if(min < max){
+		var num = (Math.random() * (max - min)) + min;
+		num = parseFloat(num.toFixed(decimals));
+		return num;		
+	}
+	else{
+		throw 'Error: randomFloat min value must be less than max value';
+	}
 }
