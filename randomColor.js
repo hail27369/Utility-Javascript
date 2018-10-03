0db0c8b28060d04070866fff3568f8ae28d65dfa
hail27369 <32968932+hail27369@users.noreply.github.com>
2018-10-03 13:48:39 -0400

    Delete randomColor.js
    merged into Util.js file

--- randomColor.js
+++
@@ -1,9 +0,0 @@
-function getRandomColor(){
-  var color = 'rgb(' + getNum0Through255() + ',' + getNum0Through255() + ',' + getNum0Through255() + ')';
-
-  return color;
-}
-
-function getNum0Through255(){
-  return Math.floor(Math.random() * 256).toString();
-}
